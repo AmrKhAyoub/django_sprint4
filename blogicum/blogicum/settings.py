d629a697ecd6bf2f44694d4b8f421eafb8df3e5f
amrkhayoub <amrkh2004njr@gmail.com>
2026-05-05 22:15:50 +0700

    P10 Mails

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -129,3 +129,7 @@ LOGOUT_REDIRECT_URL = 'blog:index'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
