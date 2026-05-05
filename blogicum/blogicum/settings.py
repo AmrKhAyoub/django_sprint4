b011baad07c51b53aa7debe89206e416860bf9ef
amrkhayoub <amrkh2004njr@gmail.com>
2026-05-05 20:19:22 +0700

    'P1 custom errors pages'

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -119,3 +119,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+CSRF_FAILURE_VIEW = 'pages.views.csrf_failure'
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
 ]
+
+handler404 = 'pages.views.page_not_found'
+handler500 = 'pages.views.server_error'
